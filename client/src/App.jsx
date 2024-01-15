30f2fbd7e63c94284d767f3f17802a9f5469ccc2
melanihorvath <144622336+melanihorvath@users.noreply.github.com>
2024-01-15 13:58:21 +0100

    Add app.jsx the stages of the fight

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
 
+import { useState } from 'react'
+
 import './App.css'
 
 function App() {
+const [landsIsSelected, setLandsIsSelected] = useState(false);
+const [pokeIsSelected, setPokeIsSelected] = useState(false);
+const [battleIsOver, setBattleIsOver] = useState(false);
+
+
 
   return (
     <>
       <div>
-
+        {/* {landsIsSelected ? EnemyPokemon}
+        EnemyPokemon ? Battle
+        Battle ?  WIN?OVER */}
       </div>
     </>
   )
