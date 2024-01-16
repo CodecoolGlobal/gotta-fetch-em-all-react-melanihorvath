8997e8273ea24a53a57362583c69bf26e5779679
Gergo Fazekas <144519526+gergofazekas92@users.noreply.github.com>
2024-01-16 19:05:21 +0100

    display enemy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,8 @@ function App() {
   const [selectedLandURL, setSelectedLandURL] = useState();
   const [readyToPlay, setReadyToPlay] = useState(false);
   const [playersPokemon, setPlayersPokemon] = useState();
-  const [area, setArea] =useState()
+  const [area, setArea] = useState();
+  const [enemy, setEnemyPokemon] = useState();
   console.log(playersPokemon)
 
 
@@ -20,10 +21,10 @@ function App() {
 
   return (
     <div>
-        { !landsIsSelected && !pokeIsSelected  &&  <SelectLands onArea={setArea} onLocationSelected={(locationURL)=>{setSelectedLandURL(locationURL) 
+        { !landsIsSelected && !pokeIsSelected  &&  <SelectLands onEnemy={setEnemyPokemon} onArea={setArea} onLocationSelected={(locationURL)=>{setSelectedLandURL(locationURL) 
           setLandsIsSelected(true)}}/>}
-        {landsIsSelected && !pokeIsSelected && !readyToPlay && <ChoosePoke onArea={area} onPokemon={setPlayersPokemon} onClickReady={setReadyToPlay}/>}
-        {readyToPlay && (<Battle onChoosenPokemon={playersPokemon} onPokeIsSelected={(pokeIsSelected) => setPokeIsSelected(pokeIsSelected)} onLandIsSelected={(landIsSelected) => setLandsIsSelected(landIsSelected)}/>)} 
+        {landsIsSelected && !pokeIsSelected && !readyToPlay && <ChoosePoke onArea={area} onEnemy={enemy} onPokemon={setPlayersPokemon} onClickReady={setReadyToPlay}/>}
+        {readyToPlay && (<Battle  readyToPlay={setReadyToPlay} onEnemy={enemy} onChoosenPokemon={playersPokemon} onPokeIsSelected={(pokeIsSelected) => setPokeIsSelected(pokeIsSelected)} onLandIsSelected={(landIsSelected) => setLandsIsSelected(landIsSelected)}/>)} 
       </div>
   )
 }
--- client/src/components/SelectLands.jsx
+++ client/src/components/SelectLands.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import UseFetch from './UseFetch';
 import DisplayLands from './DisplayLands';
-import randomArea from './RandomArea';
 
 
 function SelectLands(props) {
@@ -9,6 +8,7 @@ function SelectLands(props) {
   const { data, isPending, error } = UseFetch(apiLandsKEY)
   const [lands, setLands] = useState([])
   const area = props.onArea;
+  const enemy = props.onEnemy
 
   useEffect(() => {
     if (data) {
@@ -27,11 +27,27 @@ function SelectLands(props) {
       fetch(url)
         .then(res => res.json())
         .then(data => {
-          area(data.areas[Math.ceil(Math.random() * data.areas.length)-1].url)
+          randomEnemyUrl(data.areas[Math.ceil(Math.random() * data.areas.length)-1].url)
         })
       }
     
-  
+  function randomEnemyUrl(url){
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        enemyObj(data.pokemon_encounters[Math.ceil(Math.random() * data.pokemon_encounters.length)-1].pokemon.url)
+      })
+  }
+
+  function enemyObj(url){
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        enemy(data)
+      })
+  }
   
 
 
